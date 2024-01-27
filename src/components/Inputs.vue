5efb1ce747ab6b16b119543d25ce62e20ec40f2b
focarica <a.vinicius0042@gmail.com>
2024-01-26 23:59:35 -0300

    allow to submit 0

--- src/components/Inputs.vue
+++ src/components/Inputs.vue
@@ -56,10 +56,19 @@ export default {
         this.binaryTree = new BinaryTree();
       }
       const inputElement = Number(document.getElementById("inputNumber").value);
-      if (!inputElement && inputElement != 0 || this.binaryTree.contains(inputElement)) {
-        window.alert("Insira um valor válido ou que ainda nao foi inserido!");
+      if (
+        inputElement == null ||
+        inputElement == undefined || 
+        document.getElementById("inputNumber").value.length == 0)
+      {
+        window.alert("Insira um valor válido");
         return;
       }
+      if (this.binaryTree.contains(inputElement)) {
+        window.alert("Valor já inserido");
+        return;
+      }
+
       document.getElementById("inputNumber").value = null;
       this.binaryTree.insert(inputElement);
       this.$emit("treeFeedback", this.binaryTree);
